e66f2c9eac5f57a7abe02e8c90214bce156749ce
marenda <markostevic96@hotmail.com>
2021-03-03 19:18:37 +0100

    Create Thread Form

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/threads',[\App\Http\Controllers\ThreadsController::class,'index']);
-Route::post('/threads',[\App\Http\Controllers\ThreadsController::class,'store']);
-Route::get('/threads/{thread}',[\App\Http\Controllers\ThreadsController::class,'show']);
+Route::resource('threads',\App\Http\Controllers\ThreadsController::class);
 
 Route::post('/threads/{thread}/replies',[\App\Http\Controllers\RepliesController::class,'store']);
--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -26,6 +26,12 @@ class CreateThreadsTest extends TestCase
         $this->post('/threads',$thread->toArray());
     }
 
+    /** @test  */
+    public function guest_cannot_see_the_create_thread_page()
+    {
+        $this->get('/threads/create')->assertRedirect('/login');
+    }
+
     /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
