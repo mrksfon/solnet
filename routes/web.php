d767c3ca893fcbb0e8cac6cae16095aaf037287b
marenda <markostevic96@hotmail.com>
2021-03-08 16:30:55 +0100

    User has a Profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FavoritesController;
+use App\Http\Controllers\ProfilesController;
 use App\Http\Controllers\RepliesController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ThreadsController;
@@ -35,3 +36,5 @@ Route::get('/threads/{channel}',[ThreadsController::class,'index']);
 Route::post('/threads/{channel}/{thread}/replies',[RepliesController::class,'store']);
 
 Route::post('/replies/{reply}/favorites',[FavoritesController::class,'store'])->name('replies.favorite');
+
+Route::get('/profiles/{user}',[ProfilesController::class,'show'])->name('profile');
---
+++ tests/Feature/ProfilesTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Thread;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ProfilesTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_has_a_profile()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+
+        $this->get("/profiles/{$user->name}")->assertSee($user->name);
+    }
+
+    /** @test  */
+    public function profiles_display_all_threads_created_by_associated_user()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+
+        $thread = Thread::factory()->create(['user_id' => $user->id]);
+
+        $this->get("/profiles/{$user->name}")
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
