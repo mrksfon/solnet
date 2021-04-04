3fe05258e94dd8893840382bbe03cfccb81fcb66
marenda <markostevic96@hotmail.com>
2021-04-04 02:40:25 +0200

    Graduating Inspection Methods to Classes

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Inspections\Spam;
 use App\Models\Channel;
 use App\Models\Reply;
-use App\Models\Spam;
 use App\Models\Thread;
 use Illuminate\Http\Request;
 
--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -2,16 +2,30 @@
 
 namespace Tests\Unit;
 
-use App\Models\Spam;
+use App\Inspections\Spam;
 use Tests\TestCase;
 
 class SpamTest extends TestCase
 {
     /** @test */
-    public function it_validates_spam()
+    public function it_checks_for_invalid_keywords()
     {
          $spam = new Spam();
 
          $this->assertFalse($spam->detect('Innocent reply here'));
+
+         $this->expectException('Exception');
+
+         $spam->detect('yahoo customer support');
+    }
+
+    /** @test */
+    public function it_checks_for_any_key_being_held_down()
+    {
+        $spam = new Spam();
+
+        $this->expectException(\Exception::class);
+
+        $spam->detect('Hello world aaaaaaaaaaa');
     }
 }
